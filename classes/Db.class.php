068417372b0bc8d3312dc713513ecf2ce65a3060
Wannes Vandael <wannes.vandael@gmail.com>
2019-04-25 14:42:42 +0200

    Update DB
    Update DB file: weghalen echo's

--- classes/Db.class.php
+++ classes/Db.class.php
@@ -8,11 +8,11 @@
             //var_dump($config);
             //$this kan hier niet want er zijn geen huidige objecten meer
             if ( self::$conn != null ){
-                echo "1";
+                
                 return self::$conn;
             } 
             else {
-                echo "2";
+                
                 self::$conn = new PDO('mysql:host=localhost;dbname='.$config['db_name'], $config['db_user'], $config['db_password'],null);
                 return self::$conn;
             }

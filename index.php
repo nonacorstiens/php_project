d402a16d8fab7eef65863bf261c74bd8336745c2
nonacorstiens <nonacorstiens2@gmail.com>
2019-04-30 18:54:18 +0200

    Article for post index.php

--- index.php
+++ index.php
@@ -16,6 +16,31 @@ if (isset( $_SESSION['userid'])){
 </head>
 <body>
     <h1>Welcome</h1>
-    <a href="logout.php">logout</a>
+    <nav>
+        <a href="logout.php">logout</a>
+    </nav>
+
+    <div class="picture_grid">
+    
+        <form method="post" action="">
+            <div class="post">
+                <p class="postDescription">KIDS</p>
+                <img class="postImage" src="uploads/cropped/5cc87a1cc3a460.02144669.jpeg">
+                <br>
+                <input type="text" placeholder="Say something about this picture" id="postComment" name="postComment" />
+                <input id="btnSubmit" type="submit" value="Comment" />
+
+                <ul id="post_comment_updates">
+
+                </ul>
+                
+
+            </div>
+
+        </form>
+
+    </div>
+    
+
 </body>
 </html>
